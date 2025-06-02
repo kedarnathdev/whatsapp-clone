c5d0409261e24226a5fc000b8cbd1ad63f2555d2
Ram Kumar G <gadramkumar1@gmail.com>
2025-06-02 19:03:29 +0530

    user table created

---
+++ src/main/java/com/whatsapp/whatsapp/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.whatsapp.whatsapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.whatsapp.whatsapp.entity.UserEntity;
+import com.whatsapp.whatsapp.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	UserService service;
+	
+	@GetMapping("/getAll")
+	public List<UserEntity> getAllUsers(){
+		return service.getAllUsers();
+	}
+	
+	@GetMapping("/getAll/{id}")
+	public UserEntity getUserById(@PathVariable Integer id){
+		return service.getUserById(id);
+	}
+}
