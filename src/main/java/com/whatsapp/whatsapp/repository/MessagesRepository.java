22421778784ed697fad2922679d9b54e5b405989
Kedarnath Peraka <kedarnathperaka@gmail.com>
2025-06-02 20:13:51 +0530

    changes

--- src/main/java/com/whatsapp/whatsapp/repository/MessagesRepository.java
+++ src/main/java/com/whatsapp/whatsapp/repository/MessagesRepository.java
@@ -1,8 +1,13 @@
 package com.whatsapp.whatsapp.repository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whatsapp.whatsapp.entity.MessagesEntity;
 
 public interface MessagesRepository extends JpaRepository<MessagesEntity, Integer> {
+
+	List<MessagesEntity> findByUserId(Integer userId);
 
 }
--- src/main/java/com/whatsapp/whatsapp/repository/UserRepository.java
+++ src/main/java/com/whatsapp/whatsapp/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.whatsapp.whatsapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.whatsapp.whatsapp.entity.UserEntity;
