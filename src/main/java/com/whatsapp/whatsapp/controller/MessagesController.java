989f0757e36d15925d94be5ef6c88909f1c059b5
Kedarnath Peraka <kedarnathperaka@gmail.com>
2025-06-08 22:14:05 +0530

    major changes

--- src/main/java/com/whatsapp/whatsapp/controller/MessagesController.java
+++ src/main/java/com/whatsapp/whatsapp/controller/MessagesController.java
@@ -3,6 +3,7 @@ package com.whatsapp.whatsapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.whatsapp.whatsapp.service.MessagesService;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin("*")
 public class MessagesController {
 
 	@Autowired
